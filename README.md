2a432eabe42fc284e4b5b3025a54e3c25f1a5953
Marcus Bean <marcus.bean@outlook.com>
2024-10-17 14:21:17 -0600

    Copy rather than reference ssh keys

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ cd {containing_dir}
 docker build -t python_dev_container .
 
 ## Run the Docker container with attached volume and mounted ssh directory
-docker run -d -v python_dev_container:/home/developer -v ~/.ssh:/home/developer/.ssh -p 4000:80 python_dev_container
+docker run -d -v python_dev_container:/home/developer -v ~/.ssh:/host/.ssh -p 4000:80 python_dev_container
--- startup.sh
+++ startup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Change ownership of the .ssh directory if it exists
-if [ -d "/home/developer/.ssh" ]; then
+# Check if SSH keys exist on the host and copy them
+if [ -d "/host/.ssh" ]; then
+    cp -r /host/.ssh /home/developer/
     chown -R developer:developer /home/developer/.ssh
 fi
 
-# Run the main command (in this case, sleep infinity or any other command)
+# Run the main command
 exec "$@"
